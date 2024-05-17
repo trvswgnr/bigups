a8f3714d3256038e70c6e5a4f9c04e4c008e0403
Travis Wagner <travis.wagner@umusic.com>
2024-05-17 02:16:59 -0700

    update client, improve dx

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,16 +3,21 @@ import { Metadata, ns } from "../shared";
 const NS_PROGRESS = ns("client", "progress");
 const DEFAULT_CHUNK_SIZE = 1024 * 1024; // 1MB
 
-export default class {
+// states
+declare const Base: unique symbol;
+declare const Initialized: unique symbol;
+declare const Done: unique symbol;
+type Base = typeof Base;
+type Initialized = typeof Initialized;
+type Done = typeof Done;
+
+class BigUpsClass {
     private ws: WebSocket;
     private chunkSize: number;
     private file: File;
 
-    constructor(url: string | URL, file: File, chunkSize?: number);
-    constructor(ws: WebSocket, file: File, chunkSize?: number);
-    constructor(
+    private constructor(
         urlOrSocket: (string | URL) | WebSocket,
-        file: File,
         chunkSize = DEFAULT_CHUNK_SIZE,
     ) {
         this.ws =
@@ -20,51 +25,59 @@ export default class {
                 ? urlOrSocket
                 : new WebSocket(urlOrSocket);
         this.chunkSize = chunkSize;
-        this.file = file;
+        this.file = undefined as any;
     }
 
-    public on(
-        event: "progress",
-        listener: (event: UploadProgressEvent) => void,
-    ): void;
-    public on<E extends keyof WebSocketEventMap>(
-        event: E,
-        listener: (event: WebSocketEventMap[E]) => void,
-    ): void;
+    public static init(
+        urlOrSocket: (string | URL) | WebSocket,
+        chunkSize = DEFAULT_CHUNK_SIZE,
+    ) {
+        return new BigUpsClass(urlOrSocket, chunkSize);
+    }
+
     public on(event: string, listener: (event: any) => void) {
         if (event === "progress") {
             this.ws.addEventListener(NS_PROGRESS, listener);
-            return;
+            return this;
         }
         this.ws.addEventListener(event, listener);
+        return this;
     }
 
     public upload() {
-        this.ws.addEventListener("open", () => {
-            const ws = this.ws;
-            const metadata: Metadata = Metadata.create(this.file);
-            ws.send(Metadata.toJSON(metadata));
-            let loaded = 0;
-            const total = this.file.size;
-            const num_chunks = Math.ceil(total / this.chunkSize);
-            for (let i = 0; i < num_chunks; i++) {
-                const offset = Math.min(this.chunkSize, total - loaded);
-                const start = loaded;
-                const end = loaded + offset;
-                const chunk = this.file.slice(start, end);
-                ws.send(chunk);
-                loaded += offset;
-                const ev = new UploadProgressEvent({
-                    loaded,
-                    total,
-                    lengthComputable: true,
-                });
-                ws.dispatchEvent(ev);
-            }
+        return new Promise<BigUps<Done>>((resolve, reject) => {
+            this.ws.addEventListener("open", () => {
+                const ws = this.ws;
+                const metadata: Metadata = Metadata.create(this.file);
+                ws.send(Metadata.toJSON(metadata));
+                let loaded = 0;
+                const total = this.file.size;
+                const num_chunks = Math.ceil(total / this.chunkSize);
+                for (let i = 0; i < num_chunks; i++) {
+                    const offset = Math.min(this.chunkSize, total - loaded);
+                    const start = loaded;
+                    const end = loaded + offset;
+                    const chunk = this.file.slice(start, end);
+                    ws.send(chunk);
+                    loaded += offset;
+                    const ev = new UploadProgressEvent({
+                        loaded,
+                        total,
+                        lengthComputable: true,
+                    });
+                    ws.dispatchEvent(ev);
+                }
 
-            // close the connection
-            ws.close(1000, "success");
+                // close the connection
+                ws.close(1000, "success");
+                resolve(this);
+            });
+            this.ws.addEventListener("error", reject);
         });
+    }
+
+    public cancel() {
+        throw new Error("Not implemented");
     }
 }
 
@@ -80,3 +93,32 @@ class UploadProgressEvent extends ProgressEvent {
         this.progress = (init.loaded / init.total) * 100;
     }
 }
+
+type StateMap = {
+    [Base]: {
+        init: {
+            (url: string | URL, chunkSize?: number): BigUps<Initialized>;
+            (ws: WebSocket, chunkSize?: number): BigUps<Initialized>;
+        };
+    };
+    [Initialized]: {
+        on: {
+            (
+                event: "progress",
+                listener: (event: UploadProgressEvent) => void,
+            ): BigUps<Initialized>;
+            <E extends keyof WebSocketEventMap>(
+                event: E,
+                listener: (event: WebSocketEventMap[E]) => void,
+            ): BigUps<Initialized>;
+        };
+        upload: (file: File) => Promise<BigUps<Done>>;
+    };
+    [Done]: BigUps<Initialized>;
+};
+
+type BigUps<State extends keyof StateMap> = StateMap[State];
+
+const BigUps: BigUps<Base> = BigUpsClass;
+
+export default BigUps;
